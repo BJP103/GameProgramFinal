9aff81df9b84bb6c106a0e039f248c5746383792
BJP103 <BJP103@pitt.edu>
2025-09-07 16:56:05 -0400

    Added ammo Pickups

---
+++ Clanka Killer/Assets/Scripts/AmmoPickup.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AmmoPickup : MonoBehaviour
+{
+    public int ammoAdd = 30;
+    
+    public bool isInRange = false;
+    public Image interact;
+    public AudioSource openSound;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && isInRange == true)
+        {
+            Gun gun = GameObject.Find("WeaponHolder").GetComponentInChildren<Gun>();
+            gun.maxAmmo += ammoAdd;
+            if (openSound != null)
+                openSound.PlayOneShot(openSound.clip);
+            StartCoroutine(Despawn());
+
+        } 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        interact.gameObject.SetActive(true);
+        isInRange = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        interact.gameObject.SetActive(false);
+        isInRange = false;
+    }
+
+    IEnumerator Despawn()
+    {
+        yield return new WaitForSeconds(1);
+        Destroy(gameObject);
+    }
+}
