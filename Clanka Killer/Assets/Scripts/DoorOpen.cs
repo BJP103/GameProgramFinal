91c73f578532087d56f3b17e5fa114f779a4445f
BJP103 <BJP103@pitt.edu>
2025-11-30 17:26:39 -0500

    add door sounds

--- Clanka Killer/Assets/Scripts/DoorOpen.cs
+++ Clanka Killer/Assets/Scripts/DoorOpen.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 public class DoorOpen : MonoBehaviour
 {
     public Animator anim;
-    
+    public AudioSource openSound;  
+    public AudioSource closeSound;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player")) {
-
+            openSound.Play();
             Debug.Log("Hit " + other.gameObject.name);
             anim.SetTrigger("Open");
+            
         }
     }
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
+            closeSound.Play();
             anim.SetTrigger("Close");
     }
 }
